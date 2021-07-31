86aa9be16b729ef1db89fc9ccffd512a0a95cfd1
moony564 <71317753+moony564@users.noreply.github.com>
2021-07-31 11:10:50 +0900

    Add files via upload

---
+++ 코펭 합작.py
@@ -0,0 +1,22 @@
+import discord
+import os
+
+client = discord.Client()
+
+
+@client.event
+async def on_ready():
+
+    print(client.user.name)
+    print("start")
+    game = discord.Game('/명령어')
+    await client.change_presence(status=discord.Status.online, activity=game)
+
+
+@client.event
+async def on_message(message):
+    if message.content == "a":
+        await message.channel.send("a")
+
+access_token = os.environ["BOT_TOKEN"]
+client.run(access_token)
