f18dd85e27b8c5c61cceb9347d341eb058b517b6
moony564 <71317753+moony564@users.noreply.github.com>
2021-07-31 12:30:32 +0900

    Update bot_main.py

--- bot_main.py
+++ bot_main.py
@@ -16,7 +16,7 @@ async def on_ready():
 @client.event
 async def on_message(message):
     if message.content == "a":
-        await message.channel.send("a")
+        await message.channel.send("b")
 
 access_token = os.environ["BOT_TOKEN"]
 client.run(access_token)
